c77b76e3a28365d3166a4ae993bb0b3178988169
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-11-06 13:36:30 -0600

    Finish decorators

--- Decorators.py
+++ Decorators.py
@@ -1,8 +1,19 @@
 import time
 
 def validate_score(func):
-    pass
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if result.score < 0 or result.score > 100:
+            raise ValueError("Score must be between 0 and 100.")
+        return result
+    return wrapper
 
 def measure_time(func):
-    pass
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Evaluation time: {end - start:.2f} seconds")
+        return result
+    return wrapper
 
