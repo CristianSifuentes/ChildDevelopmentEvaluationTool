fbf4ca78c1802c5c6345a49fd0fe57cda4ef66bc
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-11-06 13:00:18 -0600

    Add initial code in Main.py and finish merged with DataHandler, Decorators, DevelopmentReport and Evaluation branches

---
+++ Main.py
@@ -0,0 +1,11 @@
+from Evaluation import CognitiveDevelopment, SocialDevelopment, EmotionalDevelopment, PhysicalDevelopment
+from DevelopmentReport import DevelopmentReport
+from DataHandler import DataHandler
+from Decorators import validate_score, measure_time
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
