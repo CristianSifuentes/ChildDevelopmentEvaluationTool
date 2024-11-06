d8dd460463d2951d472a2edcc491efb1398aee9b
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-11-06 13:16:16 -0600

    Add __init__, evaluate and __str__ method inside DevelopmentArea class

--- Evaluation.py
+++ Evaluation.py
@@ -1,8 +1,20 @@
 class DevelopmentArea:
-    pass
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score # Evaluation score on a scale, e.g., 0-100
+
+    def evaluate(self):
+        raise NotImplementedError("You must implement the evaluate method in the subclass.")
+
+    
+    def __str__(self):
+         return f"{self.name}: {self.score}/100"
 
 class CognitiveDevelopment(DevelopmentArea):
-    pass
+    
+    def evaluate(self):
+        # Evaluation based on cognitive questions or tests
+        return f"Social Evaluation: {self.score}/100"
 
 class SocialDevelopment(DevelopmentArea):
     pass
