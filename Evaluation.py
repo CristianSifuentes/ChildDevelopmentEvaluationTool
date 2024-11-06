0081706bcb8b5431b58f6fc17cf9849c236726d3
CristianSifuentes <sifuentescovarrubiasitsl@gmail.com>
2024-11-06 11:40:08 -0600

    Add empty class for initial software

---
+++ Evaluation.py
@@ -0,0 +1,15 @@
+class DevelopmentArea:
+    pass
+
+class CognitiveDevelopment(DevelopmentArea):
+    pass
+
+class SocialDevelopment(DevelopmentArea):
+    pass
+
+
+class EmotionalDevelopment(DevelopmentArea):
+    pass
+
+class PhysicalDevelopment(DevelopmentArea):
+    pass
